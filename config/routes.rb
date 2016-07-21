5d9a339ec0ec8a0937a9fb1e6a56a0fc5f3fe41e
Man Phan <phandinhman@gmail.com>
2016-07-21 11:20:31 +0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'static_pages#home'
 
-  get 'static_pages/help'
+  get 'help' => 'static_pages#help'
 
-  get 'static_pages/about'
+  get 'about' => 'static_pages#about'
 
+  get 'contact' => 'static_pages#contact'
+  
+  get 'signup' => 'users#new'
+
+  #get 'user' => 'users/id'
 
   resources :users
   #root 'users#index'
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,8 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-  	@user =  User.new(name: "Example User", email: "user@xxample.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -39,5 +40,16 @@ class UserTest < ActiveSupport::TestCase
   		assert @user.valid?, "#{valid_address.inspect} should be valid"
   	end
   end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
 
 end
